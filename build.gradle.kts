9e702c6e82a2e33fc8b699542e55be302a518dcc
Tia Firing <tia.firing@nav.no>
2020-06-26 11:54:32 +0200

    wip: syfosyketilfelle

--- build.gradle.kts
+++ build.gradle.kts
@@ -80,6 +80,7 @@ dependencies {
     implementation("io.ktor:ktor-auth-jwt:$ktorVersion")
 
     implementation("no.nav.helse:syfosm-common-kafka:$smCommonVersion")
+    implementation("no.nav.helse:syfosm-common-rest-sts:$smCommonVersion")
 
     implementation("ch.qos.logback:logback-classic:$logbackVersion")
     implementation("net.logstash.logback:logstash-logback-encoder:$logstashEncoderVersion")
---
+++ src/main/kotlin/no/nav/syfo/lagrevedtak/client/SyfoSyketilfelleClient.kt
@@ -0,0 +1,47 @@
+package no.nav.syfo.lagrevedtak.client
+
+import io.ktor.client.HttpClient
+import io.ktor.client.request.accept
+import io.ktor.client.request.get
+import io.ktor.client.request.headers
+import io.ktor.http.ContentType
+import io.ktor.util.KtorExperimentalAPI
+import java.time.LocalDate
+import no.nav.syfo.client.StsOidcClient
+
+@KtorExperimentalAPI
+class SyfoSyketilfelleClient(
+    private val syfosyketilfelleUrl: String,
+    private val stsClient: StsOidcClient,
+    private val httpClient: HttpClient
+) {
+
+    suspend fun finnStartdato(aktorId: String, sykmeldingId: String): LocalDate {
+        val sykeforloep = hentSykeforloep(aktorId)
+        return LocalDate.now()
+    }
+
+    suspend fun hentSykeforloep(aktorId: String): List<Sykeforloep> =
+        httpClient.get<SyketilfelleRespons>("$syfosyketilfelleUrl/sparenaproxy/$aktorId/sykeforloep") {
+            accept(ContentType.Application.Json)
+            val oidcToken = stsClient.oidcToken()
+            headers {
+                append("Authorization", "Bearer ${oidcToken.access_token}")
+            }
+        }.sykeforloep
+}
+
+data class SyketilfelleRespons(
+    val sykeforloep: List<Sykeforloep>
+)
+
+data class Sykeforloep(
+    var oppfolgingsdato: LocalDate,
+    val sykmeldinger: ArrayList<SimpleSykmelding>
+)
+
+data class SimpleSykmelding(
+    val id: String,
+    val fom: LocalDate,
+    val tom: LocalDate
+)
