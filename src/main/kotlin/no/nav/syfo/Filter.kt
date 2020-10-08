ce2b32461f8d4326234ce29a44706f419855087a
Tia Firing <tia.firing@nav.no>
2020-10-08 14:36:11 +0200

    endrer filter til etter 1995

--- src/main/kotlin/no/nav/syfo/Filter.kt
+++ src/main/kotlin/no/nav/syfo/Filter.kt
@@ -5,6 +5,7 @@ fun trefferAldersfilter(fnr: String, filter: Filter): Boolean {
 }
 
 enum class Filter(val regex: String) {
+    ETTER1995("\\d{4}(9[5-9]|0[0-9])\\d{5}"),
     ETTER1990("\\d{4}((0|9)[0-9])\\d{5}"),
     ETTER1980("\\d{4}((0|[8-9])[0-9])\\d{5}")
 }
--- src/test/kotlin/no/nav/syfo/FiltreringTest.kt
+++ src/test/kotlin/no/nav/syfo/FiltreringTest.kt
@@ -6,6 +6,18 @@ import org.spekframework.spek2.style.specification.describe
 
 object FiltreringTest : Spek({
 
+    describe("Test av aldersfiltrering med etter 1995-filter") {
+        it("Bruker født i 1995 gir true med etter 1995-filter") {
+            trefferAldersfilter("15069588888", Filter.ETTER1995) shouldEqual true
+        }
+        it("Bruker født i 2001 gir true med etter 1995-filter") {
+            trefferAldersfilter("15060188888", Filter.ETTER1995) shouldEqual true
+        }
+        it("Bruker født i 1994 gir false med etter 1995-filter") {
+            trefferAldersfilter("15069488888", Filter.ETTER1995) shouldEqual false
+        }
+    }
+
     describe("Test av aldersfiltrering med etter 1990-filter") {
         it("Bruker født i 1990 gir true med etter 1990-filter") {
             trefferAldersfilter("15069088888", Filter.ETTER1990) shouldEqual true
