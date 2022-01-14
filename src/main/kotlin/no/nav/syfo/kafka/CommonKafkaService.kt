de7546ebb0c54fe03b280733bc761f3538b5bd3a
Tia Firing <tia.firing@nav.no>
2022-01-14 07:10:59 +0100

    fjerner onprem-consumere for sykmelding

--- src/main/kotlin/no/nav/syfo/kafka/CommonKafkaService.kt
+++ src/main/kotlin/no/nav/syfo/kafka/CommonKafkaService.kt
@@ -3,7 +3,6 @@ package no.nav.syfo.kafka
 import kotlinx.coroutines.delay
 import no.nav.syfo.Environment
 import no.nav.syfo.aktivermelding.AktiverMeldingService
-import no.nav.syfo.aktivermelding.MottattSykmeldingService
 import no.nav.syfo.application.ApplicationState
 import org.apache.kafka.clients.consumer.KafkaConsumer
 import java.time.Duration
@@ -12,15 +11,12 @@ class CommonKafkaService(
     private val applicationState: ApplicationState,
     private val kafkaConsumer: KafkaConsumer<String, String>,
     private val env: Environment,
-    private val mottattSykmeldingService: MottattSykmeldingService,
     private val aktiverMeldingService: AktiverMeldingService
 ) {
     suspend fun start() {
         kafkaConsumer.subscribe(
             listOf(
-                env.aktiverMeldingTopic,
-                env.sykmeldingAutomatiskBehandlingTopic,
-                env.sykmeldingManuellBehandlingTopic
+                env.aktiverMeldingTopic
             )
         )
 
@@ -30,8 +26,6 @@ class CommonKafkaService(
                 if (it.value() != null) {
                     when (it.topic()) {
                         env.aktiverMeldingTopic -> aktiverMeldingService.mottaAktiverMelding(it.value())
-                        env.sykmeldingAutomatiskBehandlingTopic -> mottattSykmeldingService.mottaNySykmelding(it.value())
-                        env.sykmeldingManuellBehandlingTopic -> mottattSykmeldingService.mottaNySykmelding(it.value())
                         else -> throw IllegalStateException("Har mottatt melding på ukjent topic: ${it.topic()}")
                     }
                 }
