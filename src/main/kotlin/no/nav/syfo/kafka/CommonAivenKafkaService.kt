69adb24aa43204fa2b9681c96be3709f16029005
Karl J. Overå <k@rl.run>
2024-05-24 09:35:06 +0200

    fix: dispatch each message to a coroutine so 5s wait doesn't block messages

--- src/main/kotlin/no/nav/syfo/kafka/CommonAivenKafkaService.kt
+++ src/main/kotlin/no/nav/syfo/kafka/CommonAivenKafkaService.kt
@@ -4,7 +4,11 @@ import io.opentelemetry.api.GlobalOpenTelemetry.getTracerProvider
 import io.opentelemetry.api.trace.Tracer
 import kotlin.time.Duration.Companion.seconds
 import kotlin.time.toJavaDuration
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.joinAll
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.yield
 import no.nav.syfo.Environment
 import no.nav.syfo.aktivermelding.AktiverMeldingService
@@ -28,44 +32,50 @@ class CommonAivenKafkaService(
                 env.utbetalingTopic,
                 env.okSykmeldingTopic,
                 env.manuellSykmeldingTopic,
-                env.aktiverMeldingAivenTopic
-            )
+                env.aktiverMeldingAivenTopic,
+            ),
         )
 
         log.info("Starter Aiven Kafka consumer")
         while (applicationState.ready) {
-            val tracer: Tracer =
-                getTracerProvider().get("${CommonAivenKafkaService::class}:kafka-consumer-root")
-            val lp = tracer.spanBuilder("long-poll").startSpan()
             val records = kafkaConsumer.poll(10.seconds.toJavaDuration())
-            lp.end()
-
             if (records.isEmpty) {
                 delay(1.seconds)
             }
 
-            records.forEach {
-                val span = tracer.spanBuilder("Records.forEach").startSpan()
-                if (it.value() != null) {
-                    when (it.topic()) {
-                        env.utbetalingTopic -> utbetaltEventService.mottaUtbetaltEvent(it.value())
-                        env.okSykmeldingTopic ->
-                            mottattSykmeldingService.mottaNySykmelding(it.value())
-                        env.manuellSykmeldingTopic ->
-                            mottattSykmeldingService.mottaNySykmelding(it.value())
-                        env.aktiverMeldingAivenTopic ->
-                            aktiverMeldingService.mottaAktiverMelding(it.value())
-                        else -> {
-                            span.addEvent("Ukjent topic")
-                            span.end()
-                            throw IllegalStateException(
-                                "Har mottatt melding på ukjent topic: ${it.topic()}"
-                            )
+            val tracer: Tracer =
+                getTracerProvider().get("${CommonAivenKafkaService::class}:kafka-consumer-root")
+            val scope = CoroutineScope(Dispatchers.IO)
+
+            val jobs =
+                records.map {
+                    scope.launch {
+                        val span = tracer.spanBuilder("Records.forEach").startSpan()
+                        if (it.value() != null) {
+                            when (it.topic()) {
+                                env.utbetalingTopic ->
+                                    utbetaltEventService.mottaUtbetaltEvent(it.value())
+                                env.okSykmeldingTopic ->
+                                    mottattSykmeldingService.mottaNySykmelding(it.value())
+                                env.manuellSykmeldingTopic ->
+                                    mottattSykmeldingService.mottaNySykmelding(it.value())
+                                env.aktiverMeldingAivenTopic ->
+                                    aktiverMeldingService.mottaAktiverMelding(it.value())
+                                else -> {
+                                    span.addEvent("Ukjent topic")
+                                    span.end()
+                                    throw IllegalStateException(
+                                        "Har mottatt melding på ukjent topic: ${it.topic()}",
+                                    )
+                                }
+                            }
                         }
+                        span.end()
                     }
                 }
-                span.end()
-            }
+
+            jobs.joinAll()
+
             yield()
         }
     }
