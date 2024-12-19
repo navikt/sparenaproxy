9975442a8e1faab4c532a7276a81564647cadb04
Hein Haraldsen <hein.haraldsen@bekk.no>
2024-12-19 15:11:18 +0100

    Stopped sending of canceled 36week-messages

--- src/main/kotlin/no/nav/syfo/aktivermelding/MottattSykmeldingService.kt
+++ src/main/kotlin/no/nav/syfo/aktivermelding/MottattSykmeldingService.kt
@@ -75,8 +75,8 @@ class MottattSykmeldingService(
 
         if (
             aktiveStansmeldinger.isEmpty() &&
-            avbrutteAktivitetskravMeldinger.isEmpty() &&
-            avbrutte39ukersMeldinger.isEmpty()
+                avbrutteAktivitetskravMeldinger.isEmpty() &&
+                avbrutte39ukersMeldinger.isEmpty()
         ) {
             log.info(
                 "Fant ingen relevante planlagte meldinger knyttet til sykmeldingid $sykmeldingId",
@@ -94,9 +94,9 @@ class MottattSykmeldingService(
             "Sender ikke avbrutt 39 ukers melding $sykmeldingId",
         )
         /*     sendAvbrutt39ukersmelding(
-                 receivedSykmelding,
-                 avbrutte39ukersMeldinger.firstOrNull { it.startdato == startdato },
-             )*/
+            receivedSykmelding,
+            avbrutte39ukersMeldinger.firstOrNull { it.startdato == startdato },
+        )*/
         utsettStansmelding(
             receivedSykmelding,
             aktiveStansmeldinger.firstOrNull { it.startdato == startdato },
